aea8b6f98bbc685dc586e93aade6307e788f7937
davicaetan0 <davicaetano1004@gmail.com>
2024-11-28 16:06:56 -0300

    commit curso

--- PII/Curso.cs
+++ PII/Curso.cs
@@ -1,67 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PII
 {
     internal class Curso
     {
-        private Conexao objetoConexao = new Conexao();
+        private readonly Conexao objetoConexao = new Conexao();
 
-        private int codigo_Curso;
-        private string nome_Curso;
-
-        public int Codigo_Curso { get => codigo_Curso; set => codigo_Curso = value; }
-        public string Nome_Curso { get => nome_Curso; set => nome_Curso = value; }
+        public int CodigoCurso { get; set; }
+        public string NomeCurso { get; set; }
 
         public void Incluir()
         {
-            string sql = "";
-            sql += "Insert into Curso(nomeCurso) values ('" + Nome_Curso + "' )";
-            objetoConexao.Conectar();
-            objetoConexao.Executar(sql);
-            objetoConexao.Desconectar();
+            string sql = $"INSERT INTO Curso(nomeCurso) VALUES ('{NomeCurso}')";
+            ExecutarComando(sql);
         }
 
         public void Alterar()
         {
-            string sql = "";
-            sql = $"Update Curso set nomeCurso = '{Nome_Curso}' where idCurso = " + Codigo_Curso.ToString();
-            objetoConexao.Conectar();
-            objetoConexao.Executar(sql);
-            objetoConexao.Desconectar();
+            string sql = $"UPDATE Curso SET nomeCurso = '{NomeCurso}' WHERE idCurso = {CodigoCurso}";
+            ExecutarComando(sql);
         }
 
         public void Excluir()
         {
-            // Atualizar os registros na tabela AulaReforco para que não referenciem mais a disciplina ou curso
-            string sqlAtualizar = "UPDATE AulaReforco SET idDisciplina = NULL WHERE idDisciplina = " + Codigo_Curso.ToString();
             objetoConexao.Conectar();
-            objetoConexao.Executar(sqlAtualizar);
+            try
+            {
+                // Atualizar referências antes de excluir
+                string sqlAtualizar = $"UPDATE AulaReforco SET idDisciplina = NULL WHERE idDisciplina = {CodigoCurso}";
+                objetoConexao.Executar(sqlAtualizar);
 
-            // Agora, podemos excluir a disciplina ou curso
-            string sql = "DELETE FROM Disciplina WHERE idDisciplina = " + Codigo_Curso.ToString();
-            objetoConexao.Executar(sql);
-
-            objetoConexao.Desconectar();
+                // Excluir o curso
+                string sqlExcluir = $"DELETE FROM Disciplina WHERE idDisciplina = {CodigoCurso}";
+                objetoConexao.Executar(sqlExcluir);
+            }
+            finally
+            {
+                objetoConexao.Desconectar();
+            }
         }
-
 
         public DataSet PesquisaDados()
         {
-            string sql = "Select * from Curso";
-            objetoConexao.Conectar();
-            DataSet ds = objetoConexao.ListarDados(sql);
-            objetoConexao.Desconectar();
-            return ds;
+            string sql = "SELECT * FROM Curso";
+            return ObterDados(sql);
         }
 
+        private void ExecutarComando(string sql)
+        {
+            objetoConexao.Conectar();
+            try
+            {
+                objetoConexao.Executar(sql);
+            }
+            finally
+            {
+                objetoConexao.Desconectar();
+            }
+        }
 
-
-
-
+        private DataSet ObterDados(string sql)
+        {
+            objetoConexao.Conectar();
+            try
+            {
+                return objetoConexao.ListarDados(sql);
+            }
+            finally
+            {
+                objetoConexao.Desconectar();
+            }
+        }
     }
 }
